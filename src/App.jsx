18d461779186df49ca2add69f562912cd412cd2a
salome Nelson <salomenelson2002@gmail.com>
2025-10-22 17:19:59 +0100

    Experiences section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Hero from './components/Hero'
 import Services from "./components/Services"
 import Portfolio from "./components/Portfolio"
 
+// import Header from './components/Header'
+// import Hero from './components/Hero'
 import Experiences from './components/Experiences'
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Hero/>
       <Services/>
       <Portfolio/>
+      {/* <Header/>
+      <Hero/> */}
       <Experiences/>
     </>
   )
