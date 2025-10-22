838eb3b56b9a40e78cbff53329490702bfdb3298
salome Nelson <salomenelson2002@gmail.com>
2025-10-22 07:53:39 +0100

    Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Heros from './components/Heros'
 
 function App() {
   return (
     <>
       <Header/>
       <Hero/>
+      <Heros/>
     </>
   )
 }
