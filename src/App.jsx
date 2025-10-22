63b9342913b37e3642c1cc7d3d73daa2029483cf
salome Nelson <salomenelson2002@gmail.com>
2025-10-22 16:50:22 +0100

    Experiences section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from './components/Header'
 import Hero from './components/Hero'
-
+import Experiences from './components/Experiences'
 function App() {
   return (
     <>
       <Header/>
       <Hero/>
+      <Experiences/>
     </>
   )
 }
