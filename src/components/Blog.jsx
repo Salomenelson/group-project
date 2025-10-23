9c5044c61978cddadb65c1f423cfb443deb6a4b2
salome Nelson <salomenelson2002@gmail.com>
2025-10-23 15:10:34 +0100

    Integrated blog section from groupD folder into main folder

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -3,9 +3,8 @@ import { Blogs } from './Blogs'
 import './Blog.css'
 const Blog = () => {
   return (
-    <section className='bg-black w-full'>
-
-      <div className='contain'>
+    <section className="w-full bg-black">
+      <div className="w-[80%] max-w-[1000px] my-0 mx-auto py-15">
         <div className='text-center pb-8'>
             <div className="flex gap-2 pb-2 items-center justify-center">
               <div className="w-4 h-[2.5px] bg-gray-500"></div>
@@ -14,7 +13,7 @@ const Blog = () => {
             <h2 className="text-4xl font-bold pb-14 text-white">Personal Blog</h2>
         </div>
 
-        <div className="container">
+        <div className="md:flex gap-5">
           <Blogs imagescr={"./image1.jpg"} title="How to Prototype in figma" date="July 15, 2021" description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum animi quod, exercitationem cumque delectus iure  amet? Perferendis exercitationem blanditiis eos itaque!" reading="Continue Reading"/>
 
           <Blogs imagescr={"./image2.jpg"} title="Beign more productive in Notion" date="Aug 8, 2020" description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum animi quod, exercitationem cumque delectus iure  amet? Perferendis exercitationem blanditiis eos itaque!" reading="Continue Reading"/>
